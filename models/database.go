f8965e088ab80772de54173c01905506681f1569
Людмила <118292872+L1l1ut1kk@users.noreply.github.com>
2023-04-06 15:01:45 +0300

    Add files via upload

---
+++ models/database.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func data() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "images.db")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	defer db.Close()
+
+	// Create the Image model in the database
+	db.AutoMigrate(&Image{})
+
+	return db
+}
